42cc2d0870d4e290ffaef7976d3acdaa8a611447
BioInf2305 <BioInf2305@gmail.com>
2024-01-10 17:59:58 +0100

    added manhattan plot docker

--- optm/install_packages.r
+++ optm/install_packages.r
@@ -1,1 +1,4 @@
-install.packages("OptM")
+install.packages("remotes")
+library("remotes")
+install_version("OptM",version="0.1.6",repos="http://cran.us.r-project.org")
+install_version("optparse",version="1.7.3",repos="http://cran.us.r-project.org")
